c62fabf43da3cacea10353c71a3cec322c7d3ee6
Alejandro <ahernandezdotnet@gmail.com>
2019-03-06 20:40:35 -0600

    Implementando upload de imagenes de usuario medico y hospital

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var portNumber = 3000;
-var mongoDBPort = 27017;
+portNumber = require("./config/config").portNumber;
+mongoDBPort= require("./config/config").mongoDBPort;
 
 // Requires
 var express = require('express')
@@ -22,6 +22,7 @@ var loginRoutes = require('./routes/login')
 var hospitalRoutes = require('./routes/hospital')
 var medicoRoutes = require('./routes/medico')
 var busquedaRoutes = require('./routes/busqueda')
+var uploadRoutes = require('./routes/upload')
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:' + mongoDBPort + '/hospitalDB', { useNewUrlParser: true }, (err, res )=>{
@@ -30,13 +31,13 @@ mongoose.connection.openUri('mongodb://localhost:' + mongoDBPort + '/hospitalDB'
 } );
 
 // Rutas
+app.use('/upload', uploadRoutes)
 app.use('/busqueda', busquedaRoutes)
 app.use('/login', loginRoutes)
 app.use('/usuario', usuarioRoutes)
 app.use('/hospital', hospitalRoutes)
 app.use('/medico', medicoRoutes)
 app.use('/', appRoutes)
-
 
 // Escuchar peticiones
 app.listen(portNumber, () => { 
--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
+module.exports.portNumber = 3000;
+module.exports.mongoDBPort = 27017;
 module.exports.SEED = "@abc123xyz";
 module.exports.TIMETOEXPIRE = 144000;
 module.exports.PAGESIZE = 5;
