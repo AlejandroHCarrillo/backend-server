50568eb0911a3a95845e14a64dc04a5d280866a4
Alejandro <ahernandezdotnet@gmail.com>
2019-03-07 22:08:17 -0600

    Agregar autentificacion por google

--- models/usuario.js
+++ models/usuario.js
@@ -18,8 +18,8 @@ var usuarioSchema = new Schema({
   },
   password: { type: String, required: [true, "El password es necesario"] },
   img: { type: String, required: false },
-  role: { type: String, required: true, default: "USER_ROLE", enum: rolesValidos }
-  // , google: { type: bool, required: false }
+  role: { type: String, required: true, default: "USER_ROLE", enum: rolesValidos }, 
+  google: { type: Boolean, default: false }
 });
 
 usuarioSchema.plugin(uniqueValidator, { message: "El {PATH} debe ser unico" });
--- routes/login.js
+++ routes/login.js
@@ -10,9 +10,9 @@ var app = express();
 var Usuario = require("../models/usuario");
 
 //  Google
-const {OAuth2Client} = require('google-auth-library');
+CLIENT_ID = require("../config/config").CLIENT_ID;
+const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(CLIENT_ID);
-CLIENT_ID = require("../config/config").CLIENT_ID
 
 //  ==========================================================
 //  Autentificacion Google
@@ -20,43 +20,113 @@ CLIENT_ID = require("../config/config").CLIENT_ID
 //  ==========================================================
 async function verify(token) {
   const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    idToken: token,
+    audience: CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
   // const userid = payload['sub'];
   // If request specified a G Suite domain:
   //const domain = payload['hd'];
-  console.log(payload);
+  // console.log(payload);
 
   return {
     nombre: payload.name,
     email: payload.email,
     img: payload.picture,
     google: true
-
-
-  }
+  };
 }
 
 app.post("/google", async (req, res) => {
-
   var token = req.body.token;
-  var googleUser = await verify(token)
-                          .catch(err =>{
-                            return res.status(500).json({
-                              ok: false,
-                              mensaje: "Token no valido"
-                            });
-                          });
-
-  return res.status(200).json({
-    ok: true,
-    mensaje: "Autentificacion google jala",
-    googleUser: googleUser
+  var googleUser = await verify(token).catch(err => {
+    return res.status(403).json({
+      ok: false,
+      mensaje: "Token no valido",
+      error: err
+    });
   });
+
+  Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+    // validar si ocurrio un error
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al encontrar un usuario",
+        errors: err
+      });
+    }
+
+    //  Verifica que exista el usuario
+    if (usuarioDB) {
+      if (usuarioDB.google === false) {
+        return res.status(400).json({
+          ok: false,
+          mensaje: "Usar la autentificacion standar"
+        });
+      } else {
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, {
+          expiresIn: TIMETOEXPIRE
+        });
+
+        //   regresar respuesta
+        res.status(200).json({
+          ok: true,
+          usuario: usuarioDB,
+          token: token,
+          id: usuarioDB.id
+        });
+      }
+      // return res.status(500).json({
+      //   ok: false,
+      //   mensaje: "Credenciales incorrectas (Usuario no existe)",
+      //   errors: err
+      // });
+    } else {
+      // El usuario no existe previamente, crearlo
+      var usuario = new Usuario();
+
+      usuario.nombre = googleUser.nombre;
+      usuario.apellidoP = googleUser.nombre;
+      usuario.apellidoM = googleUser.nombre;
+      usuario.email = googleUser.email;
+      usuario.img = googleUser.img;
+      usuario.google = true;
+      usuario.password = ":S";
+
+      usuario.save((err, usuarioDB) =>{
+
+        if (err) {
+          return res.status(400).json({
+            ok: false,
+            mensaje: "Error guardando el Usuario",
+            errors: err
+          });
+        }
+    
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, {
+          expiresIn: TIMETOEXPIRE
+        });
+    
+        res.status(201).json({
+          ok: true,
+          usuario: usuarioDB,
+          token: token,
+          id: usuarioDB._id
+        });
+
+      });
+
+    }
+  });
+
+  // return res.status(200).json({
+  //   ok: true,
+  //   mensaje: "Autentificacion google OK!!",
+  //   googleUser: googleUser
+  // });
 });
 
 //  ==========================================================
@@ -65,47 +135,48 @@ app.post("/google", async (req, res) => {
 app.post("/", (req, res) => {
   var body = req.body;
 
-  Usuario.findOne( { email: body.email }, (err, usuarioDB)=>{
+  Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
     // validar si ocurrio un error
     if (err) {
-        return res.status(500).json({
-          ok: false,
-          mensaje: "Error al encontrar un usuario",
-          errors: err
-        });
-      }
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al encontrar un usuario",
+        errors: err
+      });
+    }
 
-      //  Verifica que exista el usuario
-      if(!usuarioDB){
-        return res.status(500).json({
-            ok: false,
-            mensaje: "Credenciales incorrectas (Usuario no existe)",
-            errors: err
-          });
-      }
+    //  Verifica que exista el usuario
+    if (!usuarioDB) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas (Usuario no existe)",
+        errors: err
+      });
+    }
 
-      // Verificar el password correcto
-      if( !bcrypt.compareSync(body.password, usuarioDB.password) ){
-        return res.status(200).json({
-            ok: false,
-            mensaje: "Credenciales incorrectas (Password incorrecto)",
-            errors: err
-          });
-      }
+    // Verificar el password correcto
+    if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+      return res.status(200).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas (Password incorrecto)",
+        errors: err
+      });
+    }
 
-      // Crear token que con 4 horas de vigencia
-      usuarioDB.password = ":S";
-      var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: TIMETOEXPIRE } );
+    // Crear token que con 4 horas de vigencia
+    usuarioDB.password = ":S";
+    var token = jwt.sign({ usuario: usuarioDB }, SEED, {
+      expiresIn: TIMETOEXPIRE
+    });
 
     //   regresar respuesta
-      res.status(200).json({
-          ok: true,
-          usuario: usuarioDB,
-          token: token,
-          id : usuarioDB.id
-        }); 
-  } )
-
+    res.status(200).json({
+      ok: true,
+      usuario: usuarioDB,
+      token: token,
+      id: usuarioDB.id
+    });
+  });
 });
 
 module.exports = app;
